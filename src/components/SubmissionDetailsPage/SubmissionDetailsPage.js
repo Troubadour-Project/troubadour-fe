90d2f3efe57868e45e3ce3d7ad02e2fcbabcbdfb
Emma Chory <emmachory@gmail.com>
2022-03-26 14:27:25 -0600

    Add submissiondetails route

--- src/components/SubmissionDetailsPage/SubmissionDetailsPage.js
+++ src/components/SubmissionDetailsPage/SubmissionDetailsPage.js
@@ -3,7 +3,12 @@ import "./SubmissionDetailsPage.scss";
 const SubmissionDetailsPage = () => {
   return(
     <div>
-      
+      <div className="submission-details">
+
+      </div>
+      <div className="submission-video">
+
+      </div>
     </div>
   )
 }
