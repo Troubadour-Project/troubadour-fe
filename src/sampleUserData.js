b6464c3bf9e2636517548252f131192e81f9be9e
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-25 19:26:09 -0600

    Create sample musician data

---
+++ src/sampleUserData.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    "id": 1,
+    "name": "Jessica",
+    "email": "Jessica@gmail.com",
+    "genre": "singer/songwriter",
+    "song_title": "a really good song",
+    "selected": false,
+    "type": 1,
+    "profile": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    "video": "video.url"
+  },
+  {
+    "id": 2,
+    "name": "Bo",
+    "email": "Bo@gmail.com",
+    "genre": "folk",
+    "song_title": "my song",
+    "selected": false,
+    "type": 1,
+    "profile": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    "video": "video.url"
+  },
+  {
+    "id": 3,
+    "name": "Casey",
+    "email": "Casey@gmail.com",
+    "genre": "folk",
+    "song_title": "La La La",
+    "selected": false,
+    "type": 1,
+    "profile": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    "video": "video.url"
+  }
+]
+
