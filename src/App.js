60c610a0e657d6d34f2e4fb0ffa534bb1e086bfc
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-25 19:32:36 -0600

    Update app to show admin page, will need to change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.scss';
 import NavBar from './components/NavBar/NavBar'
 import { useQuery, gql } from '@apollo/client';
+import AdminPage from './components/AdminPage/AdminPage';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <AdminPage />
+      <main>
+        {/* routes will go here */}
+      </main>
     </div>
   );
 }
