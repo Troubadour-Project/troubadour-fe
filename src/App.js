5c1ebc5222ab36172f51c1c3473e949519575b3f
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-26 12:41:17 -0600

    Install router and setup routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 import NavBar from './components/NavBar/NavBar'
 import SubmissionForm from './components/SubmissionForm/SubmissionForm';
-import { useQuery, gql } from '@apollo/client';
 import AdminPage from './components/AdminPage/AdminPage';
+import LandingPage from './components/LandingPage/LandingPage';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <main>
+        <Routes>
+          <Route exact path='/' element={<LandingPage />} />
+          <Route path='/Admin' element={<AdminPage />} />
+          <Route path='/Submission' element={<SubmissionForm />} />
+          {/* <Route path='/*' element={<Error />} /> */}
+        </Routes>
       </main>
     </div>
   );
