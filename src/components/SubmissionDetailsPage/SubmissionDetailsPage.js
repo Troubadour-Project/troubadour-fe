fa4947690dbcfea900a434589d91fc557717d68b
Emma Chory <emmachory@gmail.com>
2022-03-26 14:35:13 -0600

    Create detail and video containers

--- src/components/SubmissionDetailsPage/SubmissionDetailsPage.js
+++ src/components/SubmissionDetailsPage/SubmissionDetailsPage.js
@@ -2,12 +2,12 @@ import "./SubmissionDetailsPage.scss";
 
 const SubmissionDetailsPage = () => {
   return(
-    <div>
+    <div className="details-container">
       <div className="submission-details">
-
+        details
       </div>
       <div className="submission-video">
-
+        video
       </div>
     </div>
   )
