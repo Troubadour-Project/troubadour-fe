d222ecf4fa16f5005344c075bccc1622dcad6366
Emma Chory <emmachory@gmail.com>
2022-03-26 16:55:44 -0600

    Move details to musicianCard component

---
+++ src/components/MusicianCard/MusicianCard.js
@@ -0,0 +1,25 @@
+
+const MusicianCard = ({submissionDetails}) => {
+  submissionDetails = {
+    "id": 1,
+    "name": "Ryan",
+    "email": "ryan@gmail.com",
+    "genre": "singer/songwriter",
+    "song_title": "A Really Good Song",
+    "selected": false,
+    "type": 1,
+    "profile": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    "video": "video.url"
+  }
+
+  return(
+  <div className="submission-details">
+    <img className="details-picture" src={submissionDetails.profile} />
+    <p>{submissionDetails.name}</p>
+    <p>{submissionDetails.genre}</p>
+    <p>{submissionDetails.song_title}</p>
+  </div>
+  )
+}
+
+export default MusicianCard
