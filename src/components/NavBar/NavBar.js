78c6b505cd46cde12d706f9088b0b451dff2ff21
Ryan McBride <mr.ryan444@gmail.com>
2022-03-27 17:07:05 -0600

    Add home link to nav logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/Logo-Capstone.png'
 import './NavBar.scss'
 
 const NavBar = () => {
   return (
     <nav>
-      <img src={logo} alt="Crown of instruments"/>
+      <Link to="/"><img src={logo} alt="Crown of instruments" className="nav-logo"/></Link>
       <h1>Troubadour</h1>
       <p>| | |</p>
     </nav>
