19b1664fe7920732346d9c0236b39c2cfec0852d
Ryan McBride <mr.ryan444@gmail.com>
2022-03-27 16:22:44 -0600

    Position background image

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,15 +1,18 @@
-// import "./LandingPage.scss"
+import React from 'react'
+import heroImage from '../../assets/hero-image.jpeg'
+import "./LandingPage.scss"
 
 const LandingPage = () => {
   return(
-    <div>
-      <img></img>
-      <p>We will accept entries for the 2023 Turing Contest beginning April 1st, 2022. The entrance fee is $10 per song. Contestants may submit an original song. We will post submission details later on.
+    <div className="landing-page-container">
+      {/* <img src={heroImage} alt="Festival landscape" className="hero-image"/> */}
+      {/* <img alt="Festival landscape" className="hero-image"/> */}
+      {/* <p>We will accept entries for the 2023 Turing Contest beginning April 1st, 2022. The entrance fee is $10 per song. Contestants may submit an original song. We will post submission details later on.
 
       Important submission dates:
 
       April 22nd, 2023: All submissions must be postmarked on or before.
-      May 4th, 2023: We will notify all finalists and alternates by this date. Watch for the public posting of Troubadour finalists around this time.</p>
+      May 4th, 2023: We will notify all finalists and alternates by this date. Watch for the public posting of Troubadour finalists around this time.</p> */}
     </div>
   )
 }
