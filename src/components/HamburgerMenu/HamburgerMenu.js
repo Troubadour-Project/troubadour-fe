11b00a4578cc6966b9c42c67f4e7244d7e34f23a
Oscar Cortez <oacortez98@gmail.com>
2022-03-27 16:44:15 -0600

    Set up hamburger menu animatioon

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,57 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import './HamburgerMenu.scss';
 
-const HamburgerMenu = ({toggle}) => {
+const HamburgerMenu = ({open}) => {
+
+const displayMobileNavLinks = classNames('mobile-nav-wrapper', {
+  open: open,
+});
 
   return (
-    <div>
-    <Link to="/">
-      <button onClick={toggle}>Home</button>
-    </Link>
-    <Link to="/Admin">
-      <button onClick={toggle}>Admin Page</button>
-    </Link>
-    <Link to="/Submission">
-      <button onClick={toggle}>Submission form</button>
-    </Link>
+    <div className={displayMobileNavLinks}>
+      <Link to="/">
+        <button className="mobile-link" onClick={open}>Home</button>
+      </Link>
+      <Link to="/Admin">
+        <button className="mobile-link" onClick={open}>Admin Page</button>
+      </Link>
+      <Link to="/Submission">
+        <button className="mobile-link" onClick={open}>Submission form</button>
+      </Link>
     </div>
   )
 }
 
 export default HamburgerMenu;
-
-// <Hamburger toggled={isOpen} toggle={setOpen} />
-// https://bluegrass.com/ - Inspo site
-
-// What will this component need ?
-/**
-  User View:
-    1. <Home/> - LandingPage ?
-    2. <SubmissionPage/>
-    3. <admin page>
- */
-
-    // Factors to think about displaying this hambuger menu 
-    /**
-     - How do we want to display this hamburger menu ? 
-      - example 1:
-          Display all the button views user/admin will go.
-            Once in Mobile (responsive) Its a hambuger menu. 
-            When on click it will display all the views the user/admin can go to.
-            Youtube tutorial: https://www.youtube.com/watch?v=nLTMn19vupw
-
-      - example 2: 
-           Just display a Hamburger menu at all times 
-              - Npm package: https://negomi.github.io/react-burger-menu/ - Sidebar Animation
-
-              - Youtube tutorial: https://www.youtube.com/watch?v=P3W7MZ3JkyA - default
-              
-
-      -- Do we want to have a animation on the hamburger ?
-            ( Not necessary just for styling purposes )
-            - IF so:
-              https://hamburger-react.netlify.app/
-
-              Should I install Router ? And build out the 
-      
-     */
