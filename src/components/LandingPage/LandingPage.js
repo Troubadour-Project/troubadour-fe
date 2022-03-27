f1d1ee52beadb1949cadec394918db3f139b6c21
Ryan McBride <mr.ryan444@gmail.com>
2022-03-27 16:43:05 -0600

    Fix text positioning

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,15 +5,11 @@ import "./LandingPage.scss"
 const LandingPage = () => {
   return(
     <div className="landing-page-container">
-      {/* <img src={heroImage} alt="Festival landscape" className="hero-image"/> */}
-      {/* <img alt="Festival landscape" className="hero-image"/> */}
       <h2 className="welcome-message-header">Welcome to the Troubadour Music Contest!</h2>
-      <p className="welcome-message">We will accept entries for the 2023 Turing Contest beginning April 1st, 2022. The entrance fee is $10 per song. Contestants may submit an original song. We will post submission details later on.
-
-      Important submission dates:
-
-      April 22nd, 2023: All submissions must be postmarked on or before.
-      May 4th, 2023: We will notify all finalists and alternates by this date. Watch for the public posting of Troubadour finalists around this time.</p>
+      <p className="welcome-message">We will accept entries for the 2023 Troubadour Music Contest beginning April 1st, 2022. Contestants may submit an original song. We will post submission details later on.</p>
+      <p className="submission-dates-header">Important submission dates:</p>
+      <p className="submission-dates-april">April 22nd, 2023: All submissions must be postmarked on or before.</p>
+      <p className="submission-dates-may">May 4th, 2023: We will notify all finalists and alternates by this date. Watch for the public posting of Troubadour finalists around this time.</p>
     </div>
   )
 }
