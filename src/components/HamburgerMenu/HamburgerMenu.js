0970eab0eec2fb7c5a6405fd5f384d4948425cc1
Ryan McBride <mr.ryan444@gmail.com>
2022-03-28 16:25:52 -0600

    Remove commented out code

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -11,15 +11,6 @@ const displayMobileNavLinks = classNames('mobile-nav-wrapper', {
 
   return (
     <div className={displayMobileNavLinks}>
-      {/* <Link to="/">
-        <button className="mobile-link" onClick={() => setMobileNavOpen(!open)}>Home</button>
-      </Link>
-      <Link to="/submission">
-        <button className="mobile-link" onClick={(open) => setMobileNavOpen(!open)}>Submission form</button>
-      </Link>
-      <Link to="/admin">
-        <button className="mobile-link" onClick={(open) => setMobileNavOpen(!open)}>admin page</button>
-      </Link> */}
       <Link to="/" className="mobile-link" onClick={() => setMobileNavOpen(!open)}>Home</Link>
       <Link to="/submission" className="mobile-link" onClick={() => setMobileNavOpen(!open)}>Submission Form</Link>
       <Link to="/admin" className="mobile-link" onClick={() => setMobileNavOpen(!open)}>Admin</Link>
