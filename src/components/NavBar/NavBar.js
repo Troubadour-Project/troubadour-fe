b86f3b8a3b72ed205c0cd32390953e8bd7a326ed
Ryan McBride <mr.ryan444@gmail.com>
2022-03-27 18:19:02 -0600

    Revert nav link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import logo from '../../assets/Logo-Capstone.png'
 import './NavBar.scss'
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/"><img src={logo} alt="Crown of instruments" className="nav-logo"/></Link>
+      <img src={logo} alt="Crown of instruments" className="nav-logo"/>
       <h1>Troubadour</h1>
       <p>| | |</p>
     </nav>
