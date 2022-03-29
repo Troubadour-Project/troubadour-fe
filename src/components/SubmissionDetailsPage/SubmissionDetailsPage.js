7a210171382e56f507acf29ebb0242658ab70b4f
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-28 19:06:59 -0600

    Add useParams and update component layout based on what I think query will return

--- src/components/SubmissionDetailsPage/SubmissionDetailsPage.js
+++ src/components/SubmissionDetailsPage/SubmissionDetailsPage.js
@@ -1,16 +1,32 @@
 import "./SubmissionDetailsPage.scss";
 import "../MusicianProfile/MusicianProfile";
-import MusicianCard from "../MusicianProfile/MusicianProfile";
+import MusicianProfile from "../MusicianProfile/MusicianProfile";
+import { useQuery } from '@apollo/client';
+// import { GET_SINGLE_SUBMISSION } from '../../queries';
+import { useParams } from 'react-router-dom';
 
+const SubmissionDetailsPage = ({ user }) => {
+  const { id } = useParams();
+  console.log(id)
 
-const SubmissionDetailsPage = () => {
-  return(
-    <div className="details-container">
-      <MusicianCard />
-      <div className="submission-video">
-        video
-      </div>
-    </div>
+  // const { loading, error, data } = useQuery(GET_SINGLE_SUBMISSION, {
+  //   variables: { id },
+  // });
+
+  // const voteButton = user && <button>Winner</button>
+
+  // if (data) return(
+  //   <div className="details-container">
+  //     <MusicianProfile data={data}/>
+  //     <div className="submission-video">
+  //       video
+  //     </div>
+  //     { voteButton }
+  //   </div>
+  // );
+
+  return (
+    <p>Submission details will go here</p>
   )
 }
 
