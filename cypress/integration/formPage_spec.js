8891d693d34dd16839011981d5279d20faacdf0e
Ryan McBride <mr.ryan444@gmail.com>
2022-04-02 13:05:33 -0600

    Add tests for form page navigation bar

---
+++ cypress/integration/formPage_spec.js
@@ -0,0 +1,86 @@
+describe('Submission Form Page User Flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/form')
+  });
+
+  it('Should have a navigation bar', () => {
+    cy.get('nav')
+      .should('exist')
+  });
+
+  it('Should have a logo', () => {
+    cy.get('img')
+      .should('have.attr', 'src', '/static/media/Logo-Capstone.f486188199165f3fcb37.png')
+  });
+
+  it('Should have a title', () => {
+    cy.get('h1')
+      .should('have.text', 'Troubadour')
+  });
+
+  it('Should have a login button', () => {
+    cy.get('.login-button')
+      .should('exist')
+      .should('have.text', 'Login')
+  });
+
+  it('Should click the Login button and see the text change', () => {
+    cy.get('.login-button')
+      .wait(1000)
+      .click()
+      .get('.login-button')
+      .should('have.text', 'Logout')
+  });
+
+  it('Should have a hamburger menu', () => {
+    cy.get('.mobile-nav-wrapper')
+      .should('exist')
+  });
+
+  it('Should open the hamburger menu', () => {
+    cy.get('.mobile-menu')
+      .wait(1000)
+      .click()
+      .wait(1000)
+      .get('.open')
+      .should('exist')
+  });
+
+  it('Should have a list of links in the expanded menu', () => {
+    cy.get('.mobile-menu')
+      .click()
+      .wait(1000)
+      .get('.mobile-link')
+      .should('have.length', 3)
+      .get('.mobile-link')
+      .first()
+      .should('have.text', 'Home')
+      .get('.mobile-link')
+      .eq(1)
+      .should('have.text', 'Submission Form')
+      .get('.mobile-link')
+      .eq(2)
+      .should('have.text', 'All Submissions')
+  });
+
+  it('Should click the Submission Form link', () => {
+    cy.get('.mobile-menu')
+      .click()
+      .wait(1000)
+      .get('.mobile-link')
+      .eq(1)
+      .click()
+      .url().should('eq', 'http://localhost:3000/form')
+  });
+
+  it('Should click the All Submissions link', () => {
+    cy.get('.mobile-menu')
+      .click()
+      .wait(1000)
+      .get('.mobile-link')
+      .eq(2)
+      .click()
+      .url().should('eq', 'http://localhost:3000/submissions')
+  });
+
+});
