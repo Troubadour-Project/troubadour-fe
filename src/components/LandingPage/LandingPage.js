3f02d4afecf833c944311385f30f1a8612b74191
Ali Roemhildt <aliroemhildt@gmail.com>
2022-04-01 21:57:32 -0600

    Fetch winner and display on landing page load

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./LandingPage.scss"
+import { useQuery } from '@apollo/client'
+import { GET_WINNER } from '../../queries'
 
 const LandingPage = () => {
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    refetch()
+      .then(response => {
+        console.log(response.data.getWinner)
+        setWinner(response.data.getWinner)
+      })
+  }, [])
+
+  const { loading, error, data, refetch } = useQuery(GET_WINNER)
+  
+  const displayWinner = winner &&
+    <p className="winner">{data.getWinner.name} is the winner!!!!!!!</p>
+
   return(
     <div className="landing-page-container">
+      { displayWinner }
       <h2 className="welcome-message-header">Welcome to the Troubadour Music Contest!</h2>
       <p className="welcome-message">We will accept entries for the 2023 Troubadour Music Contest beginning April 1st, 2022. Contestants may submit an original song. We will post submission details later on.</p>
       <p className="submission-dates-header">Important submission dates:</p>
