5635b0f4dfc4248b2aa46d92964f410b80ac8147
Emma Chory <emmachory@gmail.com>
2022-04-03 14:12:00 -0600

    Add star icon to musician card

--- src/components/MusicianCard/MusicianCard.js
+++ src/components/MusicianCard/MusicianCard.js
@@ -1,10 +1,12 @@
 import './MusicianCard.scss';
 import { Link } from 'react-router-dom';
+import starIcon from '../../assets/star-icon.png'
 
 const MusicianCard = ({ submission }) => {
   return (
     <Link to={`/submissions/${submission.id}`} key={submission.id}>
       <div className='card' >
+        <img src={starIcon} className='star-icon' />
         <img
           src={`${submission.profileUrl}`}
           className='profile-img'
@@ -15,5 +17,5 @@ const MusicianCard = ({ submission }) => {
     </Link>
   );
 }
- 
+
 export default MusicianCard;
