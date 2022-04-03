6db931493eacd69982e4e798732f8c812ad4ea5e
Ali Roemhildt <aliroemhildt@gmail.com>
2022-04-03 00:42:14 -0600

    Create modal component and add styling

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,52 @@
+import { gql, useMutation } from "@apollo/client";
+import './Modal.scss';
+
+const UPDATE_WINNER = gql`
+  mutation ($id: ID!, $winner: Boolean!) {
+    updateWinner(input: {id: $id, winner: $winner}) {
+      submission {
+        id
+        name
+        email
+        genre
+        songTitle
+        winner
+      }
+    }
+  }
+`
+
+const Modal = ({ name, setShowModal, id }) => {
+  const [selectWinner, { loading, error, data }] = useMutation(UPDATE_WINNER, {
+    variables: { id: id, winner: true }
+  })
+
+  const updateWinner = (event) => {
+    selectWinner()
+      .then(response => console.log(response.data.updateWinner.submission))
+      .then(() => closeModal())
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
+  return (
+    <div className="modal-background">
+      <div className="modal-container">
+        <div className="x-button-container">
+          <button className="x-button" onClick={() => closeModal()}>X</button>
+        </div>
+        <div className="modal-text-container">
+          <p className="modal-text">Are you sure you want to select {`${name}`} as the Troubadour winner?</p>
+        </div>
+        <div className="confirm-buttons-container">
+          <button className="confirm-button" onClick={() => updateWinner()}>Select Winner</button>
+          <button className="confirm-button" onClick={() => closeModal()}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
