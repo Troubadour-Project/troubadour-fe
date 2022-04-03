3a2536de338412079c476d726220d368be92567a
Ryan McBride <mr.ryan444@gmail.com>
2022-04-03 15:54:29 -0600

    Add and pass winner selection tests

--- cypress/integration/admin_spec.js
+++ cypress/integration/admin_spec.js
@@ -1,56 +1,54 @@
 describe('Admin Flow', () => {
   beforeEach(() => {
-    // cy.intercept('POST', 'https://troubadour-be.herokuapp.com/graphql', { fixture: 'submission-details-response.json' }).as('submission-details-response')
-      cy.visit('http://localhost:3000/submissions/1')
-        .wait(2000)
-      // .wait('@submission-details-response')
+    cy.visit('http://localhost:3000/submissions/1')
+      .wait(2000)
   });
 
-  // it('Should have a navigation bar', () => {
-  //   cy.get('nav')
-  //     .should('exist')
-  // });
+  it('Should have a navigation bar', () => {
+    cy.get('nav')
+      .should('exist')
+  });
 
-  // it('Should have a logo', () => {
-  //   cy.get('img')
-  //     .should('have.attr', 'src', '/static/media/Logo-Capstone.f486188199165f3fcb37.png')
-  // });
+  it('Should have a logo', () => {
+    cy.get('img')
+      .should('have.attr', 'src', '/static/media/Logo-Capstone.f486188199165f3fcb37.png')
+  });
 
-  // it('Should have a title', () => {
-  //   cy.get('h1')
-  //     .should('have.text', 'Troubadour')
-  // });
+  it('Should have a title', () => {
+    cy.get('h1')
+      .should('have.text', 'Troubadour')
+  });
 
-  // it('Should have a hamburger menu', () => {
-  //   cy.get('.mobile-nav-wrapper')
-  //     .should('exist')
-  // });
+  it('Should have a hamburger menu', () => {
+    cy.get('.mobile-nav-wrapper')
+      .should('exist')
+  });
 
-  // it('Should open the hamburger menu', () => {
-  //   cy.get('.mobile-menu')
-  //     .wait(1000)
-  //     .click()
-  //     .wait(1000)
-  //     .get('.open')
-  //     .should('exist')
-  // });
+  it('Should open the hamburger menu', () => {
+    cy.get('.mobile-menu')
+      .wait(1000)
+      .click()
+      .wait(1000)
+      .get('.open')
+      .should('exist')
+  });
 
-  // it('Should have a list of links in the expanded menu', () => {
-  //   cy.get('.mobile-menu')
-  //     .click()
-  //     .wait(1000)
-  //     .get('.mobile-link')
-  //     .should('have.length', 3)
-  //     .get('.mobile-link')
-  //     .first()
-  //     .should('have.text', 'Home')
-  //     .get('.mobile-link')
-  //     .eq(1)
-  //     .should('have.text', 'Submission Form')
-  //     .get('.mobile-link')
-  //     .eq(2)
-  //     .should('have.text', 'All Submissions')
-  // });
+  it('Should have a list of links in the expanded menu', () => {
+    cy.get('.mobile-menu')
+      .click()
+      .wait(1000)
+      .get('.mobile-link')
+      .should('have.length', 3)
+      .get('.mobile-link')
+      .first()
+      .should('have.text', 'Home')
+      .get('.mobile-link')
+      .eq(1)
+      .should('have.text', 'Submission Form')
+      .get('.mobile-link')
+      .eq(2)
+      .should('have.text', 'All Submissions')
+  });
 
   it('Should have a button on the submission details page to select a winner', () => {
     cy.intercept('POST', 'https://troubadour-be.herokuapp.com/graphql', { fixture: 'admin-response.json' }).as('admin-response')
@@ -108,5 +106,27 @@ describe('Admin Flow', () => {
       .click()
       .get('.modal-container')
       .should('not.exist')
+  });
+
+  it('Should select a winner', () => {
+    cy.intercept('POST', 'https://troubadour-be.herokuapp.com/graphql', { fixture: 'admin-response.json' }).as('admin-response')
+      .get('.login-button')
+      .wait(1000)
+      .click({ force: true })
+      .wait('@admin-response')
+      .get('button')
+      .contains('Select As Winner')
+      .click()
+      .intercept('POST', 'https://troubadour-be.herokuapp.com/graphql', { fixture: 'update-winner-response.json' }).as('update-winner-response')
+      .get('.modal-container')
+      .should('exist')
+      .contains('Select Winner')
+      .click()
+      .wait('@update-winner-response')
+      .intercept('POST', 'https://troubadour-be.herokuapp.com/graphql', { fixture: 'get-winner-response.json' }).as('get-winner-response')
+      .visit('http://localhost:3000/')
+      .wait('@get-winner-response')
+      .get('.winner')
+      .should('have.text', 'sub1 is the winner!!!!!!!')
   });
 });
