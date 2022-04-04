1600c696548bf1d3c3437c2281645564d0782298
Ryan McBride <mr.ryan444@gmail.com>
2022-04-03 20:14:10 -0600

    Add message to modal

--- src/components/UploadingModal/UploadingModal.js
+++ src/components/UploadingModal/UploadingModal.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './UploadingModal.scss'
 
 const UploadingModal = props => {
   return (
     <section className="uploading-modal-background">
       <section className="uploading-modal-container">
-
+        <section className="uploading-text-container">
+          <p className="">Uploading your Submission. This might take some time.</p>
+        </section>
+        <section className="uploading-button-container">
+          <Link to="/">Home</Link>
+          <Link to={`/submissions/${props.id}`}>Your Submission</Link>
+        </section>
       </section>
     </section>
   )
