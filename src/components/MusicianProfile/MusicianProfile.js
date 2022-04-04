3265e512c72160e83984b778cc0233e1deedec10
Oscar Cortez <oacortez98@gmail.com>
2022-04-04 13:59:53 -0600

    Fix postion layout for muscian profile details

--- src/components/MusicianProfile/MusicianProfile.js
+++ src/components/MusicianProfile/MusicianProfile.js
@@ -8,9 +8,9 @@ const MusicianProfile = ({ submission }) => {
       <img className="details-picture" src={submission.profileUrl} />
     </div>
     <div className="details-card-container">
-      <p className="submission-detail">{submission.name}</p>
-      <p className="submission-detail">{submission.genre}</p>
-      <p className="submission-detail">{submission.songTitle}</p>
+      <p className="submission-detail">Musician Name: {submission.name}</p>
+      <p className="submission-detail">Genre: {submission.genre}</p>
+      <p className="submission-detail">Song Title: {submission.songTitle}</p>
     </div>
   </div>
   )
