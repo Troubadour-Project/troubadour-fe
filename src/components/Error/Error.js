ab7b9748498ed036510917fa50ee58d237d49528
Ali Roemhildt <aliroemhildt@gmail.com>
2022-04-05 13:40:19 -0600

    Add if blocks to handle each type of error:

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,13 +1,36 @@
 import { Link } from 'react-router-dom';
 import './Error.scss';
 
-const Error = ({ message }) => {
+const Error = ({ error }) => {
+  let message = '';
+  let showHomeButton = true;
+
+  if (!error) {
+    message = 'Looks like that page does not exist.';
+  }
+  
+  if (error.graphQLErrors) {
+    message = 'Something went wrong, please try again.';
+  }
+  
+  if (error.clientErrors) {
+    message = 'Sorry, we couldn\'t find that. Please try again.';
+  }
+  
+  if (error.networkError) {
+    message = 'We\'re having trouble on our end. Please try again later.'
+    showHomeButton = false;
+  }
+
+  const homeButton = showHomeButton &&
+    <Link to='/'>
+      <button className="home-button">Back To Home Page</button>
+    </Link>
+
   return (
     <div className="error-container">
       <p className="error-message">{ message }</p>
-      <Link to='/'>
-        <button className="home-button">Back To Home Page</button>
-      </Link>
+      { homeButton }
     </div>
   )
 }
