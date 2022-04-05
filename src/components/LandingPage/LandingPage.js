d963fce91f72b5e5fc55c04b0f3ba391c3bec951
Ali Roemhildt <aliroemhildt@gmail.com>
2022-04-05 13:41:02 -0600

    Adjust layout of winner name:

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import "./LandingPage.scss"
 import { useQuery } from '@apollo/client'
 import { GET_WINNER } from '../../queries'
+import UploadingSpinner from '../UploadingSpinner/UploadingSpinner';
+import Error from '../Error/Error';
+import "./LandingPage.scss"
 
 const LandingPage = () => {
   const [winner, setWinner] = useState(null);
@@ -15,9 +17,14 @@ const LandingPage = () => {
   const { loading, error, data, refetch } = useQuery(GET_WINNER)
   
   const displayWinner = winner &&
-    <p className="winner">{data.getWinner.name} is the winner!!!!!!!</p>
-
-  return(
+    <div className="winner-container">
+      <p className="winner-title">Our Troubadour 2022 winner is:</p>
+      <p className="winner-name">{data.getWinner.name}</p>
+    </div>
+  
+  if (loading) return <UploadingSpinner />
+  if (error) return <Error error={error}/>
+  if (data) return(
     <div className="landing-page-container">
       { displayWinner }
       <h2 className="welcome-message-header">Welcome to the Troubadour Music Contest!</h2>
