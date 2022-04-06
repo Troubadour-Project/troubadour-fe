cd4f51b6a14e88a3c0499eec66cca199116f374f
Ryan McBride <mr.ryan444@gmail.com>
2022-04-05 21:53:34 -0600

    Conditionally render favorite submissions

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -5,36 +5,59 @@ import { useQuery } from '@apollo/client';
 import { GET_SUBMISSIONS } from '../../queries';
 
 const AdminPage = ({ user }) => {
-  const [data, setData] = useState()
-  const { loading, error, refetch } = useQuery(GET_SUBMISSIONS, {onCompleted: setData});
-  
+  // const [data, setData] = useState([])
+  const [isFilteredSelected, setIsFilteredSelected] = useState(false)
+  const { loading, error, data, refetch } = useQuery(GET_SUBMISSIONS);
+  console.log(data)
   useEffect(() => {
     refetch()
   }, [])
 
-  const filterFavorites = () => {
-    setData({getSubmissions: data.getSubmissions.filter(sub => sub.adminFavorite)})
-  }
+  // const filterFavorites = () => {
+  //   setData({getSubmissions: data.getSubmissions.filter(sub => sub.adminFavorite)})
+  // }
 
   if (loading || !data) return 'Loading...';
   if (error) return <p>error: {error.message}</p>;
   if (data) {
-    const renderCards = (data) => {
-      if (data.getSubmissions.length > 0) {
-        return data.getSubmissions.map(submission => {
-          return <MusicianCard refetch={refetch}  user={user} key={submission.id} submission={submission} />
-        });
-      }
+    // const renderCards = (data) => {
+    //   if (data.getSubmissions.length > 0) {
+    //     return data.getSubmissions.map(submission => {
+    //       return <MusicianCard refetch={refetch}  user={user} key={submission.id} submission={submission} />
+    //     });
+    //   }
+    // }
+    // const handleFavorites = data => {
+    //   setIsFilteredSelected(true)
+    //   const filteredFavorites = data.getSubmissions.filter(submission => submission.adminFavorite)
+    //   return filteredFavorites.map(submission => {
+    //     return <MusicianCard refetch={refetch} user={user} key={submission.id} submission={submission}/>
+    //   })
+    // }
+
+    const handleAllSubmissions = () => {
+      setIsFilteredSelected(false)
     }
+    const handleFavorites = () => {
+      setIsFilteredSelected(true)
+    }
+
+    const allSubmissions = data.getSubmissions.map(submission => {
+      return <MusicianCard refetch={refetch} user={user} key={submission.id} submission={submission}/>
+    })
+    const filteredSubmissions = data.getSubmissions.filter(submission => submission.adminFavorite).map(submission => {
+      return <MusicianCard refetch={refetch} user={user} key={submission.id} submission={submission}/>
+    })
 
     const showFilter = user && <div>
       <label>View All</label>
-      <input type="radio" name="filter" defaultChecked onChange={renderCards} />
+      <input type="radio" name="filter" defaultChecked onChange={() => handleAllSubmissions()} />
       <label>View Favorites</label>
-      <input type="radio" name="filter" onChange={filterFavorites}  />
+      {/* <input type="radio" name="filter" onChange={() => filterFavorites()}  /> */}
+      <input type="radio" name="filter" onChange={() => handleFavorites()}  />
     </div>
 
-
+    const cardsForDisplay = isFilteredSelected ? filteredSubmissions : allSubmissions
     return(
       <div className='admin-page'>
         {showFilter}
@@ -42,7 +65,7 @@ const AdminPage = ({ user }) => {
           <h2 className='admin-title'>Submissions</h2>
         </div>
         <div className='card-container'>
-          { renderCards(data) }
+          { cardsForDisplay }
         </div>
       </div>
     );
