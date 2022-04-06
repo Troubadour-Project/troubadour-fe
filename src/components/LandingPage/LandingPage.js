e8dea50731b7fc30a7aac5e15f28020a4522bf4c
Oscar Cortez <oacortez98@gmail.com>
2022-04-06 14:44:10 -0600

    Remove commented code

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -58,9 +58,4 @@ const LandingPage = () => {
   )
 }
 
-
-{/* <p className="welcome-message">We will accept entries for the 2023 Troubadour Music Contest beginning April 1st, 2022. Contestants may submit an original song. We will post submission details later on.</p>
-        <p className="submission-dates-header">Important submission dates:</p>
-        <p className="submission-dates-april">April 22nd, 2023: All submissions must be postmarked on or before.</p>
-        <p className="submission-dates-may">May 4th, 2023: We will notify all finalists and alternates by this date. Watch for the public posting of Troubadour finalists around this time.</p> */}
 export default LandingPage
